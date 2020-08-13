790d0c5cc84fee58b730e25824cc965e80738b5f
Lucas Vidigal <lucas@pop-os.localdomain>
2020-08-13 10:26:48 -0300

    Iniciando mobile first - Page Register

--- web/src/pages/Register/index.tsx
+++ web/src/pages/Register/index.tsx
@@ -1,7 +1,29 @@
 import React from  'react';
 
+import logoImg from '../../assets/images/logo.svg';
+
+import './styles.css';
+
 function Register(){
-    return <h1>Register</h1>
+    return (
+        <div id="page-register">
+            <div id="banner">
+                <div id="logo-container">
+                    <img src={logoImg} alt="Logotipo"/>
+                    <h2>Sua plataforma de estudos online.</h2>
+                </div>
+            </div>
+            
+            <form>
+                <h1>Cadastro</h1>
+                <div>
+                    <span>Prencha os dados abaixo para começar.</span>
+
+                    <input type="text" placeholder="Nome"/>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default Register;
